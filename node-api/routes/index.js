b00bc259b9ee76f4451508c67020d22bddb3fa02
Rishab <rishabarora2008@Gmail.com>
2020-05-30 23:20:58 +0530

    testing router added

--- node-api/routes/index.js
+++ node-api/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+router.get('/hello', (req, res, next) => {
+  res.send('Hello :D');
 });
 
 module.exports = router;
