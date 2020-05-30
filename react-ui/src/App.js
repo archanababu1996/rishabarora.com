9cc223542a25e9f1dff301ded4a76cd6f81074ad
Dimple Arora <dimps.capri@gmail.com>
2020-05-30 21:16:12 +0530

    trying react using npm

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          The site is coming soon!
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://www.youtube.com/channel/UCjo4sZ-G8ExZpGATmUrKKkA"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Subscribe to Technocratz
+          Learn React
         </a>
       </header>
     </div>
--- react-ui/src/App.test.js
+++ react-ui/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders self promotion link', () => {
+test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/subscribe to technocratz/i);
+  const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
