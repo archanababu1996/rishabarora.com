b2ea2eadc6d3bee04b2a6f0fc870ced9163d4c2a
Rishab <rishabarora2008@Gmail.com>
2020-05-30 17:59:25 +0530

    self promotion :p

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The site is coming soon!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/channel/UCjo4sZ-G8ExZpGATmUrKKkA"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Subscribe to Technocratz
         </a>
       </header>
     </div>
